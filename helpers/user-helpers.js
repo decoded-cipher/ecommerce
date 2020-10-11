6b42df89f0414407b98952e53aa661f87c3a6ac4
Arjun Krishna <arjun12345krishna@gmail.com>
2020-10-11 08:33:00 +0530

    Login Created Successful:

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -10,5 +10,29 @@ module.exports = {
                 resolve(data.ops[0])
             })
         })
+    },
+
+    doLogin: (userData) => {
+        return new Promise(async(resolve, reject) => {
+            var loginStatus = false
+            var response = {}
+            var user = await db.get().collection(collection.USER_COLLECTION).findOne({ Email : userData.Email })
+            if (user) {
+                bcrypt.compare(userData.Password, user.Password).then((status) => {
+                    if(status) {
+                        console.log("Login Success");
+                        response.user = user
+                        response.status = true
+                        resolve(response)
+                    } else {
+                        console.log("Login Failed");
+                        resolve({status : false})
+                    }
+                })
+            } else {
+                console.log("Login Failed : User doesnot exist");
+                resolve({status : false})
+            }
+        })
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
@@ -14,19 +15,27 @@ router.get('/', function (req, res, next) {
   })
 });
 
-router.get('/login', (req, res) => {
-  res.render('user/login')
-})
-
-
 router.get('/signup', (req, res) => {
   res.render('user/signup')
 })
 
-
 router.post('/signup', (req, res) => {
   userHelpers.doSignUp(req.body).then((response) => {
     console.log(response);
+  })
+})
+
+router.get('/login', (req, res) => {
+  res.render('user/login')
+})
+
+router.post('/login', (req, res) => {
+  userHelpers.doLogin(req.body).then((response) => {
+    if (response.status) {
+      res.redirect('/')
+    } else {
+      res.redirect('/login')
+    }
   })
 })
 
