c4d8f2343dc3777539c6aacc16bfb3ba0708c189
Arjun Krishna <arjun12345krishna@gmail.com>
2020-10-11 09:04:56 +0530

    Session created:

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,12 @@ const userHelpers = require('../helpers/user-helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  var user = req.session.user
+  console.log(user);
 
   productHelpers.getAllProducts().then((products) => {
-    console.log(products);
-    res.render('user/view-products', {
-      products
-    })
+    // console.log(products);
+    res.render('user/view-products', { products, user })
   })
 });
 
@@ -32,11 +32,20 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
   userHelpers.doLogin(req.body).then((response) => {
     if (response.status) {
+      
+      req.session.loggedIn = true
+      req.session.user = response.user
+
       res.redirect('/')
     } else {
       res.redirect('/login')
     }
   })
+})
+
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 })
 
 module.exports = router;
