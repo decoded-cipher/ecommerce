3c7fb8424e231e89f2804c35ee51e5d82aac2d58
Arjun Krishna <arjun12345krishna@gmail.com>
2020-10-12 16:02:37 +0530

    Cart Count using AJAX:

--- routes/user.js
+++ routes/user.js
@@ -73,9 +73,11 @@ router.get('/cart', verifyLogin, async(req, res) => {
   res.render('user/cart', {products, user: req.session.user})
 })
 
-router.get('/add-to-cart/:id', verifyLogin, (req, res) => {
+router.get('/add-to-cart/:id', (req, res) => {
+  console.log("API Call");
   userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
-    res.redirect('/')
+    // res.redirect('/')
+    res.json({ status: true })
   })
 })
 
