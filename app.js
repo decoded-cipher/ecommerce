a7f33040b333c864ea53c4f0e0e71860c45ca71a
Arjun Krishna <arjun12345krishna@gmail.com>
2020-10-12 13:45:31 +0530

    Cart Design Complete:

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(fileUpload())
-app.use(session({secret: "Key", cookie: {maxAge: 600000}}))
+app.use(session({secret: "Key", cookie: {maxAge: 6000000}}))
 
 db.connect((err) => {
   if (err) {
